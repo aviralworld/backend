84b107e806e32af2ce82f9258f692700d1aad73e
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-08-04 19:55:23 +0530

    refactor(db)!: create `Db` trait

--- src/db.rs
+++ src/db.rs
@@ -1,29 +1,64 @@
-use sqlx::{self, postgres::PgPool, Postgres, QueryAs};
-use uuid::Uuid;
+use futures::future::BoxFuture;
 
 use crate::errors::BackendError;
 use crate::recording::{NewRecording, RecordingMetadata};
 
-pub async fn insert_recording_metadata(
-    metadata: RecordingMetadata,
-    pool: &PgPool,
-) -> Result<NewRecording, BackendError> {
-    use sqlx::prelude::*;
+pub trait Db {
+    fn insert(&self, metadata: RecordingMetadata) -> BoxFuture<Result<NewRecording, BackendError>>;
+}
 
-    let query: QueryAs<Postgres, (Uuid,)> = sqlx::query_as(include_str!("queries/create.sql"));
+pub use self::postgres::*;
 
-    let (id, ) = query
-        .bind(&metadata.age)
-        .bind(&metadata.gender)
-        .bind(&metadata.location)
-        .bind(&metadata.name)
-        .bind(&metadata.occupation)
-        .bind(&metadata.created)
-        .bind(&metadata.category)
-        .bind(&metadata.unlisted)
-        .fetch_one(pool)
-        .await
-        .map_err(|e| BackendError::Sqlx { source: e })?;
+mod postgres {
+    use futures::future::BoxFuture;
+    use sqlx::{self, postgres::PgPool, Postgres, QueryAs};
+    use time::OffsetDateTime;
+    use uuid::Uuid;
 
-    Ok(NewRecording::new(id, metadata))
+    use crate::errors::BackendError;
+    use crate::recording::{NewRecording, RecordingMetadata};
+
+    pub struct PgDb {
+        pool: PgPool,
+    }
+
+    impl PgDb {
+        pub fn new(pool: PgPool) -> Self {
+            PgDb { pool }
+        }
+    }
+
+    impl super::Db for PgDb {
+        fn insert(
+            &self,
+            metadata: RecordingMetadata,
+        ) -> BoxFuture<Result<NewRecording, BackendError>> {
+            use futures::FutureExt;
+
+            insert(metadata, &self.pool).boxed()
+        }
+    }
+
+    async fn insert(
+        metadata: RecordingMetadata,
+        pool: &PgPool,
+    ) -> Result<NewRecording, BackendError> {
+        use sqlx::prelude::*;
+
+        let query: QueryAs<Postgres, (Uuid, OffsetDateTime, OffsetDateTime)> = sqlx::query_as(include_str!("queries/create.sql"));
+
+        let (id, created_at, updated_at) = query
+            .bind(&metadata.age_id)
+            .bind(&metadata.gender_id)
+            .bind(&metadata.location)
+            .bind(&metadata.name)
+            .bind(&metadata.occupation)
+            .bind(&metadata.category_id)
+            .bind(&metadata.unlisted)
+            .fetch_one(pool)
+            .await
+            .map_err(|e| BackendError::Sqlx { source: e })?;
+
+        Ok(NewRecording::new(id, created_at, updated_at, metadata))
+    }
 }
