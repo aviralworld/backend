4c6190a578fe6406663f2b1ebe15ba35ce7cfd20
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-08-04 19:54:13 +0530

    fix(SQL)!: rename foreign key columns to `*_id`

--- src/queries/create.sql
+++ src/queries/create.sql
@@ -1,2 +1,2 @@
 -- PostgreSQL returns the SQLSTATE code 23505 in case of a unique key constraint violation.  
-INSERT INTO recordings (id, age, gender, location, name, occupation, url, parent, category, unlisted) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id;
+INSERT INTO recordings (id, age_id, gender_id, location, name, occupation, url, parent_id, category_id, unlisted) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id, created_at, updated_at;
