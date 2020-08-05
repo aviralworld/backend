1f1606bb0833bdb61bb61b88601e7bf815d120c0
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-08-06 01:21:58 +0530

    fix!: add joins to `retrieve.sql` (and reformat)

--- src/queries/retrieve.sql
+++ src/queries/retrieve.sql
@@ -1,1 +1,18 @@
-SELECT age, gender, location, name, occupation, url, created, parent, category, unlisted FROM recordings WHERE id = $1 LIMIT 1;
+SELECT recordings.id,
+       recordings.url,
+       recordings.created_at,
+       recordings.updated_at,
+       recordings.unlisted,
+       recordings.parent_id,
+       recordings.name,
+       recordings.location,
+       recordings.occupation,
+       categories.label AS category,
+       ages.label AS age,
+       genders.label AS gender
+FROM recordings
+LEFT JOIN categories ON categories.id = recordings.category_id
+LEFT JOIN ages ON ages.id = recordings.age_id
+LEFT JOIN genders ON genders.id = recordings.gender_id
+WHERE recordings.id = $1
+LIMIT 1;
