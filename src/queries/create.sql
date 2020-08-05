7100bdd6f6b87d65dee7a289ca209b7710435a8b
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-08-06 01:18:12 +0530

    style: format `create.sql` using `sqlformat`

--- src/queries/create.sql
+++ src/queries/create.sql
@@ -1,2 +1,6 @@
--- PostgreSQL returns the SQLSTATE code 23505 in case of a unique key constraint violation.  
-INSERT INTO recordings (id, url, category_id, parent_id, unlisted, name, location, occupation, age_id, gender_id) VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id, created_at, updated_at;
+-- PostgreSQL returns the SQLSTATE code 23505 in case of a unique key constraint violation.
+
+INSERT INTO recordings (id, url, category_id, parent_id, unlisted, name, location, occupation, age_id, gender_id)
+VALUES (uuid_generate_v4(), $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id,
+                                                                          created_at,
+                                                                          updated_at;
