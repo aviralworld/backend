e116d4c19e15b83054ba3ed53d4087ee82c2009f
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-08-06 18:15:53 +0530

    fix(create.sql): re-add ID fields and reformat

--- src/queries/retrieve.sql
+++ src/queries/retrieve.sql
@@ -1,18 +1,21 @@
-SELECT recordings.id,
-       recordings.url,
-       recordings.created_at,
-       recordings.updated_at,
-       recordings.unlisted,
-       recordings.parent_id,
-       recordings.name,
-       recordings.location,
-       recordings.occupation,
-       categories.label AS category,
-       ages.label AS age,
-       genders.label AS gender
-FROM recordings
-LEFT JOIN categories ON categories.id = recordings.category_id
-LEFT JOIN ages ON ages.id = recordings.age_id
-LEFT JOIN genders ON genders.id = recordings.gender_id
-WHERE recordings.id = $1
+SELECT "recordings"."id",
+       "recordings"."url",
+       "recordings"."created_at",
+       "recordings"."updated_at",
+       "recordings"."category_id",
+       "recordings"."unlisted",
+       "recordings"."parent_id",
+       "recordings"."name",
+       "recordings"."age_id",
+       "recordings"."gender_id",
+       "recordings"."location",
+       "recordings"."occupation",
+       "categories"."label" AS "category",
+       "ages"."label" AS "age",
+       "genders"."label" AS "gender"
+FROM "recordings"
+LEFT JOIN "categories" ON "categories"."id" = "recordings"."category_id"
+LEFT JOIN "ages" ON "ages"."id" = "recordings"."age_id"
+LEFT JOIN "genders" ON "genders"."id" = "recordings"."gender_id"
+WHERE "recordings"."id" = $1
 LIMIT 1;
