4341d49d16b9cc6c5ea6afe68af140362bcdf73f
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-08-11 23:14:06 +0530

    fix: always sort children by creation date

--- src/queries/retrieve_children.sql
+++ src/queries/retrieve_children.sql
@@ -1,1 +1,1 @@
-SELECT "id", "name" FROM "recordings" WHERE "parent_id" = $1 AND "deleted_at" IS NULL AND "unlisted" = FALSE;
+SELECT "id", "name" FROM "recordings" WHERE "parent_id" = $1 AND "deleted_at" IS NULL AND "unlisted" = FALSE ORDER BY "created_at" ASC;
