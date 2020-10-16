156509c79176c0c630ef439c432a0ac0c23a280a
Shiv Jha-Mathur <4199779-shivjm@users.noreply.gitlab.com>
2020-10-16 08:52:09 +0530

    fix: add `formats` route to main router

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let environment = Environment::new(logger, db, urls, store, checker);
 
+    let formats_route = routes::make_formats_route(environment.clone());
     let count_route = routes::make_count_route(environment.clone());
     let upload_route = routes::make_upload_route(environment.clone());
     let children_route = routes::make_children_route(environment.clone());
@@ -47,7 +48,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let retrieve_route = routes::make_retrieve_route(environment.clone());
     let hide_route = routes::make_hide_route(environment.clone());
 
-    let routes = count_route
+    let routes = formats_route
+        .or(count_route)
         .or(upload_route)
         .or(children_route)
         .or(delete_route)
